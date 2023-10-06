3ff4e8b98b78cd739d2b87856c843cf475fc54f6
Jaycee Buarao <jayceebuarao@gmail.com>
2023-10-05 17:10:26 -0700

    groceries.dart initial commit

---
+++ lib/screens/groceries.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GroceriesScreen extends StatefulWidget {
+  const GroceriesScreen({super.key});
+
+  @override
+  State<GroceriesScreen> createState() => _GroceriesScreenState();
+}
+
+class _GroceriesScreenState extends State<GroceriesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
