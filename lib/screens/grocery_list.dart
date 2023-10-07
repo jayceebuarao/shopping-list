63eb489a7aab2865b415fb6c85008019985a9226
Jaycee Buarao <jayceebuarao@gmail.com>
2023-10-06 21:01:50 -0700

    initiate get data from firebasedb

--- lib/screens/grocery_list.dart
+++ lib/screens/grocery_list.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/screens/new_item.dart';
 import 'package:shopping_list/widgets/grocery_list_item.dart';
+
+import 'package:http/http.dart' as http;
 
 class GroceryListScreen extends StatefulWidget {
   const GroceryListScreen({super.key});
@@ -11,19 +16,43 @@ class GroceryListScreen extends StatefulWidget {
 }
 
 class _GroceryListScreenState extends State<GroceryListScreen> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
 
-  void _addItem() async {
-    final newItem = await Navigator.of(context).push<GroceryItem>(
-        MaterialPageRoute(builder: (ctx) => const NewItem()));
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
 
-    if (newItem == null) {
-      return;
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-8a968-default-rtdb.firebaseio.com', 'shopping-list.json');
+
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere((catItem) =>
+              catItem.value.categorytitle == item.value['category'])
+          .value;
+      loadedItems.add(GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category));
     }
 
     setState(() {
-      _groceryItems.add(newItem);
+      _groceryItems = loadedItems;
     });
+  }
+
+  void _addItem() async {
+    await Navigator.of(context).push<GroceryItem>(
+        MaterialPageRoute(builder: (ctx) => const NewItem()));
+
+    _loadItems();
   }
 
   void _removeItem(GroceryItem item) {
